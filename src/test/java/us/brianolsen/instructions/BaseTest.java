a9296f770aaf3b2e33507b32ef02a56a449a99a0
Brian Olsen <BOlsen@trustwave.com>
2017-12-24 07:49:05 -0600

    Comment most fixtures test until files open bug is fixed.

--- src/test/java/us/brianolsen/instructions/BaseTest.java
+++ src/test/java/us/brianolsen/instructions/BaseTest.java
@@ -1,13 +1,19 @@
 package us.brianolsen.instructions;
 
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonParser;
 import com.mapbox.services.api.directions.v5.models.LegStep;
+
+import us.brianolsen.instructions.util.ResourceUtil;
 
 public class BaseTest {
 	protected static final double DELTA = 1E-10;
@@ -15,6 +21,9 @@ public class BaseTest {
 	protected static final String LANGUAGE = OSRMTextInstructions.DEFAULT_LANGUAGE;
 	protected static final String MODULE_NAME = OSRMTextInstructions.MODULE_NAME;
 	protected static final String MODULE_VERSION = OSRMTextInstructions.MODULE_VERSION;
+	protected static final String FIXTURES_DIRECTORY = Paths.get( //
+			ResourceUtil.getNodeModuleDirectory(MODULE_NAME, MODULE_VERSION).getAbsolutePath(), //
+			"test", "fixtures", VERSION).toString();
 
 	public class FixtureModel {
 		private LegStep step;
@@ -42,5 +51,26 @@ public class BaseTest {
 		}
 
 		return body;
+	}
+
+	protected void testFixture(String fixture) {
+		String fixtureDirectory = Paths.get(FIXTURES_DIRECTORY, fixture).toString();
+		File fixtureDirectoryFile = new File(fixtureDirectory);
+		for (File fixtureFile : fixtureDirectoryFile.listFiles()) {
+
+			String body = loadJsonFixture(fixtureFile.getAbsolutePath());
+			FixtureModel model = new Gson().fromJson(body, FixtureModel.class);
+			System.out.println(fixture + ":" + fixtureFile.getName());
+			for (Object entry : model.getInstructions().entrySet()) {
+				try (OSRMTextInstructions textInstructions = new OSRMTextInstructions(VERSION)) {
+					Map.Entry<String, String> pair = (Map.Entry<String, String>) entry;
+					String language = pair.getKey();
+					String compiled = pair.getValue();
+					assertEquals(compiled, textInstructions.compile(language, model.getStep(), null));
+				} catch (RuntimeException e) {
+					System.err.println(e);
+				}
+			}
+		}
 	}
 }
